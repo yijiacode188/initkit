66a12c4d2bcfc0e14e67cca2bbb007db79f46e4f
yijia663856 <1275848510@qq.com>
2026-02-02 16:19:57 +0800

    添加参数获取方式

--- lib/conf.go
+++ lib/conf.go
@@ -2,6 +2,7 @@ package lib
 
 import (
 	"github.com/spf13/viper"
+	"time"
 )
 
 var viperConf *viper.Viper
@@ -18,6 +19,11 @@ func initViperConf(configPath string) error {
 	return nil
 }
 
+// IsSetConf 是否设置了key
+func IsSetConf(key string) bool {
+	return viperConf.IsSet(key)
+}
+
 // GetStringConf 获取配置
 func GetStringConf(key string) string {
 	if key == "" {
@@ -26,7 +32,55 @@ func GetStringConf(key string) string {
 	return viperConf.GetString(key)
 }
 
-// IsSetConf 是否设置了key
-func IsSetConf(key string) bool {
-	return viperConf.IsSet(key)
+// GetStringMapConf 获取get配置信息
+func GetStringMapConf(key string) map[string]interface{} {
+	if key == "" {
+		return nil
+	}
+	conf := viperConf.GetStringMap(key)
+	return conf
+}
+
+// GetConf 获取get配置信息
+func GetConf(key string) interface{} {
+	conf := viperConf.Get(key)
+	return conf
+}
+
+// GetBoolConf 获取get配置信息
+func GetBoolConf(key string) bool {
+	conf := viperConf.GetBool(key)
+	return conf
+}
+
+// GetFloat64Conf 获取get配置信息
+func GetFloat64Conf(key string) float64 {
+	conf := viperConf.GetFloat64(key)
+	return conf
+}
+
+// GetIntConf 获取get配置信息
+func GetIntConf(key string) int {
+	conf := viperConf.GetInt(key)
+	return conf
+}
+
+// GetStringMapStringConf 获取get配置信息
+func GetStringMapStringConf(key string) map[string]string {
+
+	conf := viperConf.GetStringMapString(key)
+	return conf
+}
+
+// GetStringSliceConf 获取get配置信息
+func GetStringSliceConf(key string) []string {
+
+	conf := viperConf.GetStringSlice(key)
+	return conf
+}
+
+// GetTimeConf 获取get配置信息
+func GetTimeConf(key string) time.Time {
+	conf := viperConf.GetTime(key)
+	return conf
 }
